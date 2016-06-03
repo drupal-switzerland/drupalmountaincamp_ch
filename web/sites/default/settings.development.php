3a7f79c7f03b6bb556164cf589f17f86386c447b
Alexander Tkachev <alex.leksat@gmail.com>
2016-06-03 12:10:52 +0300

    NEWSITE-58 Add amazee.io drupal config files.

---
+++ web/sites/default/settings.development.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * AmazeeIO Drupal 8 development environment configuration file.
+ *
+ * This file will only be included on development environments.
+ */
+ // Disable Google Analytics from sending dev GA data.
+$config['google_analytics.settings']['account'] = 'UA-XXXXXXXX-YY';
