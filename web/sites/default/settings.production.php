71a7677ac44c8b0339defa02b40cb43282d4b1cb
Tyler Ward <twardnw@gmail.com>
2016-07-06 15:41:26 -0700

    Update for use of correct versions of AMAZEEIO_SITE_URL and AMAZEEIO_BASE_URL

--- web/sites/default/settings.production.php
+++ web/sites/default/settings.production.php
@@ -8,8 +8,8 @@
 
 ### If the Base URL of Production is different then AMAZEEIO_SITE_URL (like runs on https or without a www in front).
 ### Adapt it here:
-if (getenv('AMAZEEIO_SITE_URL')) {
-  $base_url = 'http://www.' . getenv('AMAZEEIO_SITE_URL');
+if (getenv('AMAZEEIO_BASE_URL')) {
+  $base_url = getenv('AMAZEEIO_BASE_URL');
 }
 
 ### Trusted Host Patterns for production, see https://www.drupal.org/node/2410395 for more information.
@@ -18,7 +18,7 @@ if (getenv('AMAZEEIO_SITE_URL')) {
 ### In case the site runs on another URL, change it like:
 ###   '^www\.example\.com$',
 $settings['trusted_host_patterns'] = array(
-  '^www\.' . str_replace('.', '\.', getenv('AMAZEEIO_SITE_URL')) . '$',
+  '^' . str_replace('.', '\.', getenv('AMAZEEIO_SITE_URL')) . '$',
 );
 
 # On production, we enable the cache and the css and js aggregation.
