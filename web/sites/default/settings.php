63e2012a3883b244aac3354f9ce74e7d8ba7e8e1
Elliot Schlegelmilch <elliot@amazee.io>
2017-05-26 15:32:36 -0700

    update order of explode arguments for varnish: AMAZEEIO-684

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -56,7 +56,7 @@ if (getenv('AMAZEEIO_SOLR_HOST') && getenv('AMAZEEIO_SOLR_PORT')) {
 
 ### amazee.io Varnish & Reverse proxy settings
 if (getenv('AMAZEEIO_VARNISH_HOSTS') && getenv('AMAZEEIO_VARNISH_SECRET')) {
-    $varnish_hosts = explode(getenv('AMAZEEIO_VARNISH_HOSTS'), ',');
+    $varnish_hosts = explode(',', getenv('AMAZEEIO_VARNISH_HOSTS'));
     array_walk($varnish_hosts, function (&$value, $key) {
         $value .= ':6082';
     });
