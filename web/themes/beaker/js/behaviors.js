4ff0a4c008a00ab7ffca8ee83466686ddeeb2686
Vijay Dubb <VD39@users.noreply.github.com>
2019-02-03 15:38:09 +0100

    Update JS

--- web/themes/beaker/js/behaviors.js
+++ web/themes/beaker/js/behaviors.js
@@ -1,83 +1,56 @@
-(function ($) {
-  /**
-  * Get CSS Breakpoints
-  * Usage: window.breakpoint gives you the actual breakpoint
-  * E.g.: if(window.breakpoint == 'mobile') { Your Code }
-  */
-  Drupal.behaviors.getBreakpointsFromCSS = {
-    attach: function(context, settings) {
-      var breakpoint;
-      var breakpoint_refreshValue;
-      breakpoint_refreshValue = function () {
-        window.breakpoint = window.getComputedStyle(document.querySelector('html'), ':before').getPropertyValue('content').replace(/\"/g, '');
-      };
-
-      $(window).resize(function () {
-        breakpoint_refreshValue();
-      }).resize();
-    }
-  };
-
+(function($) {
   Drupal.behaviors.menuToggle = {
     attach: function(context, settings) {
-      $('.menu-toggle').on('click',function(){
-        $('.menu-toggle a').toggleClass('expanded');
-        $('.menu--main').parents('header').toggleClass('expanded');
-      });
+      function setLabels($element, label) {
+        $element
+          .attr('aria-label', label)
+          .find('.visually-hidden')
+          .text(label);
+      }
 
-    }
+      var openText = Drupal.t('Open menu');
+      var closeText = Drupal.t('Close menu');
+
+      $('.menu-toggle__button').on('click', function() {
+        $('.menu-toggle__icon')
+          .toggleClass('expanded')
+          .parents('header')
+          .toggleClass('expanded');
+
+        if ($('.menu-toggle__icon').hasClass('expanded')) {
+          setLabels($(this), closeText);
+          $('.main-menu-wrapper .menu a')
+            .first()
+            .focus();
+        } else {
+          setLabels($(this), openText);
+        }
+      });
+    },
   };
 
-
   /*
-* Slow scroll to Anchor
-* - https://css-tricks.com/snippets/jquery/smooth-scrolling/
-*/
-
-Drupal.behaviors.accordionActiveTrail = {
-   attach : function(context, settings) {
-    if ($('.path-frontpage')) {
-      $(function() {
-        $('.menu--main a[href*="#"]:not([href="#"])',context).click(function(e) {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html, body').animate({
-                scrollTop: target.offset().top -80
-              }, 1000);
-              //Use this rather than return false
-              e.preventDefault();
+   * Slow scroll to Anchor
+   * - https://css-tricks.com/snippets/jquery/smooth-scrolling/
+   */
+  Drupal.behaviors.accordionActiveTrail = {
+    attach: function(context, settings) {
+      if ($('.path-frontpage')) {
+        $(function() {
+          $('.menu--main a[href*="#"]:not([href="#"])', context).click(function(e) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                $('html, body')
+                  .animate({scrollTop: target.offset().top - 80}, 1000);
+                //Use this rather than return false
+                e.preventDefault();
+              }
             }
-          }
+          });
         });
-      });
-    }
-  }
-};
-
-
-/*
-* Sticky Header
-* - On scroll to certain length header beomes sticky
-*/
-Drupal.behaviors.stickyHeader = {
-  attach: function (context, settings) {
-
-
-    supaScroll = function(){
-      if ($(window).scrollTop() >= 50) {
-        $('.header').addClass('sticky');
-      } else {
-        $('.header').removeClass('sticky');
       }
-    }
-
-    $(window).scroll(function(){
-      supaScroll();
-    });
-
-  }
-};
-
+    },
+  };
 })(jQuery);
